2e81e9fc0b18be42b98bdcb13ff90576a2fa4da2
Piotr Wereszczyński <6324814+YourSenseiCreeper@users.noreply.github.com>
2024-04-20 12:24:22 +0200

    Thread safety unit test

---
+++ src/LiczbyNaSlowaNetCoreTests/ThreadSafetyTest.cs
@@ -0,0 +1,46 @@
+﻿using LiczbyNaSlowaNETCore;
+using System.Threading;
+using Xunit;
+
+namespace LiczbyNaSlowaNetCoreTests
+{
+    public class ThreadSafetyTest
+    {
+        [Fact]
+        public void IsNuberToTextThreadSafe()
+        {
+            bool failed = false;
+            int iterations = 100;
+            const string correctResponse = "jeden tysiąc dziewięćset dziewięćdziesiąt dziewięć";
+            // threads interact with some object - either 
+            Thread thread1 = new Thread(new ThreadStart(delegate () {
+                for (int i = 0; i < iterations; i++)
+                {
+                    var result = NumberToText.Convert(1999m, stems: true); // call unsafe code
+                    // check that object is not out of synch due to other thread
+                    if (!result.Equals(correctResponse))
+                    {
+                        failed = true;
+                    }
+                }
+            }));
+            Thread thread2 = new Thread(new ThreadStart(delegate () {
+                for (int i = 0; i < iterations; i++)
+                {
+                    var result = NumberToText.Convert(1999m, stems: true); // call unsafe code
+                    // check that object is not out of synch due to other thread
+                    if (!result.Equals(correctResponse))
+                    {
+                        failed = true;
+                    }
+                }
+            }));
+
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            Assert.False(failed, "code was thread safe");
+        }
+    }
+}
